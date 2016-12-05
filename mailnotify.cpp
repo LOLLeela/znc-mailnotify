c287a3cca2f107111620698f265c293588d48fbb
leela <leela@leela-ross.com>
2016-12-05 12:30:53 +1100

    removed notification sent to user

--- mailnotify.cpp
+++ mailnotify.cpp
@@ -613,12 +613,13 @@ class CNotifoMod : public CModule
 				msg += ": " + message;
 
 				bool sent = send_message(msg, title, nick.GetNick());
-
-				if (sent) {
+				/**
+ 				if (sent) {
 					PutIRC("PRIVMSG " + nick.GetNick() + " : [znc] User not connected. Notification message sent.");
 				} else {
 					PutIRC("PRIVMSG " + nick.GetNick() + " : [znc] User not connected. Notification message failed to send.");
 				}
+				*/
 			}
 
 			return CONTINUE;
